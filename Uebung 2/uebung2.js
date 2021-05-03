c17f2de8e43933e6b3b3e63f2ef3ff8b9aecad74
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2021-05-03 11:34:06 +0200

    vocabulary

--- Uebung 2/uebung2.js
+++ Uebung 2/uebung2.js
@@ -409,7 +409,7 @@ function getInputValue(){
     }*/
     if(isValid(document.getElementById("input").value) == true){ // Checks whether the input is valid
         if((JSON.parse(document.getElementById("input").value)).type != "LineString"){
-            document.getElementById("errorMessage").innerHTML = 'ERROR: This is not a LineString. <br> Expected form: {"type":"LineString","coordinates":[...]}'
+            document.getElementById("errorMessage").innerHTML = 'ERROR: This is not a LineString. Expected pattern: {"type":"LineString","coordinates":[...]}'
         } else{
             linestring = JSON.parse(document.getElementById("input").value)
             main(linestring)
