be00305614d8f7e6c92d87ce93f473c34aec9fa9
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2021-05-04 19:20:17 +0200

    Disign on html

--- Uebung 3/lib/uebung_3.js
+++ Uebung 3/lib/uebung_3.js
@@ -54,7 +54,7 @@ function apiRequest(){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             // Typical action to be performed when the document is ready:
-            document.getElementById("demo").innerHTML = xhttp.responseText;
+            //document.getElementById("demo").innerHTML = xhttp.responseText;
             response = JSON.parse(xhttp.responseText);
         }
     };
@@ -74,4 +74,34 @@ function main(){
     setTimeout(() => {
         console.log(response);
     }, 2000);
+}
+//source: http://www.sternwarte-eberfing.de/Aktuell/Himmelsrichtung.html (degree classifications)
+var degreesList = [[22.5,"N"],[67.5,"NE"],[112.5,"E"],[157.5,"SE"],[202.5,"S"],[247.5,"SW"],[292.5,"W"],[337.5,"NW"],[360.0,"N"]];
+
+/**
+*@function {toDirection} Function to convert bearing from degree to string
+*@param {double} degrees
+*@returns {String} returns bearing as a string
+*/
+function toDirection(degrees){
+	for(var i=0; i<degreesList.length; i++){
+		if(degrees<degreesList[i][0]) {
+            return degreesList[i][1];
+        }
+	}
+}
+
+function submit(){
+    getAPIKey();
+    main();
+    setTimeout(() => {
+        document.getElementById("source").innerHTML = "<b>Quelle: </b>" + response.alerts[0].sender_name;
+        document.getElementById("timezone").innerHTML = "<b>Zeitzone: </b>" + response.timezone;
+        document.getElementById("temp").innerHTML = "<b>Temperatur: </b>" + response.current.temp + " °C";
+        document.getElementById("feels_like").innerHTML = "<b>Gefühlte Temperatur: </b>" + response.current.feels_like + " °C";
+        document.getElementById("humidity").innerHTML = "<b>Luftfeuchtigkeit: </b>" + response.current.humidity + " %";
+        document.getElementById("pressure").innerHTML = "<b>Luftdruck: </b>" + response.current.pressure + " hPa";
+        document.getElementById("wind").innerHTML = "<b>Wind: </b> " + toDirection(response.current.wind_deg) + " " + response.current.wind_speed + " km/h";
+    }, 2000);
+    //document.getElementById("temp").innerHTML = "<b>Temperatur: </b>" + response.current.temp;
 }
