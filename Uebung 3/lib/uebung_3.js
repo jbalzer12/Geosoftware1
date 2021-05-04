0b402552581be4b3e271d3b80ae37be6fce2f710
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2021-05-04 10:59:54 +0200

    geolocate microsoft

--- Uebung 3/lib/uebung_3.js
+++ Uebung 3/lib/uebung_3.js
@@ -1,13 +1,31 @@
-var x = document.getElementById("geoLocation");
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+  
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
 }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude; 
+function error() {
+    status.textContent = 'Unable to retrieve your location';
 }
+
+function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+
+    if(!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+}
+  
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
