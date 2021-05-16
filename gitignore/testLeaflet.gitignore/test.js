9537657390948565858d240bc0e5f5f423830000
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2021-05-16 18:59:44 +0200

    gitignore

--- gitignore/testLeaflet.gitignore/test.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-var osmLayer = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-	{attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map)
-
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
