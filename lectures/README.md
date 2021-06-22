ea09ad436a0da47c32a9336440e2a9b0b2b329b3
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2021-06-22 12:03:21 +0200

    tidy up

--- lectures/README.md
+++
@@ -1,3 +0,0 @@
-# Geosoft2021
-
-Repository with examples used to prepare the Geosoft I lecture (SoSe 2021) at the Institute for Geoinformatics, University of Münster.
--- lectures/lecture 08 (express-simple)/routes/2_add.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET add page. */
-router.get('/', function(req, res, next) {
-  res.render('2_add', { title: 'Add' });
-});
-
-module.exports = router;
